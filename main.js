bf998783bf9d3376975ae5110996508fa87d6d22
danieldamian09 <humbertorivero9@gmail.com>
2022-06-12 17:51:58 -0300

    Add card article pizza

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // Variables
-const formulario = document.querySelector('#formulario');
+const formulario = document.querySelector('#form-data');
 const idPizza = document.querySelector('#idPizza');
 
 
@@ -8,36 +8,42 @@ const pizzas = [
 	{
 		id: 1,
 		nombre: "Pepperoni",
+		imagen: "./assets/img/peperoni.jpg",
 		ingredientes: ["pepperoni", "queso", "tomate", "pimenton", "harina"],
 		precio: 800,
 	},
 	{
 		id: 2,
 		nombre: "Hawaiana",
+		imagen: "./assets/img/hawaiana.jpg",
 		ingredientes: ["queso", "tomate", "cebolla", "pimenton", "piña", "harina"],
 		precio: 900,
 	},
 	{
 		id: 3,
 		nombre: "Vegetariana",
+		imagen: "./assets/img/vegetariana.jpg",
 		ingredientes: ["tomate", "cebolla", "rucula", "pimenton", "harina"],
 		precio: 700,
 	},
 	{
 		id: 4,
 		nombre: "Napolitana",
+		imagen: "./assets/img/napolitana.jpg",
 		ingredientes: ["queso", "tomate", "cebolla", "pimenton", "jamon", "harina"],
 		precio: 1000,
 	},
 	{
 		id: 5,
 		nombre: "4 quesos",
+		imagen: "./assets/img/cuatro-quesos.jpg",
 		ingredientes: ["mozzarella", "parmesano", "cheddar", "provolone", "harina"],
 		precio: 1200,
 	},
 	{
 		id: 6,
 		nombre: "Mozzarella",
+		imagen: "./assets/img/mozzarella.jpg",
 		ingredientes: ["mozzarella", "tomate", "harina"],
 		precio: 500,
 	},
@@ -54,6 +60,12 @@ window.addEventListener("load", () => {
   localStorage.setItem("pizzas", JSON.stringify(pizzas));
 });
 
+
+// Crear Card Pizzas
+const crearCard = (pizza) => {
+	const card = document.createElement('div');
+	card.classList.add('card');
+}
 
 
 formulario.addEventListener("submit", (e) => {
