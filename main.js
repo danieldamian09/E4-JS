b272c59891e6d9d582131731a90a53adcda26350
danieldamian09 <humbertorivero9@gmail.com>
2022-06-12 20:13:30 -0300

    Feat add pizza by ID

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // Variables
-const formulario = document.querySelector('#form-data');
-const idPizza = document.querySelector('#idPizza');
+const formulario = document.querySelector("#form-data");
+const idPizza = document.querySelector("#idPizza");
+const container = document.querySelector(".card-container");
 
 
 // Data de la pizza
@@ -49,7 +50,6 @@ const pizzas = [
 	},
 ];
 
-
 // Guardar array al localStorage
 // window.onload = function () {
 //   localStorage.setItem("pizzas", JSON.stringify(pizzas));
@@ -57,42 +57,115 @@ const pizzas = [
 
 // Guardar array al localStorage
 window.addEventListener("load", () => {
-  localStorage.setItem("pizzas", JSON.stringify(pizzas));
+	localStorage.setItem("pizzas", JSON.stringify(pizzas));
 });
-
 
 // Crear Card Pizzas
 const crearCard = (pizza) => {
-	const card = document.createElement('div');
-	card.classList.add('card');
-}
+	// Crear la card
+	const card = document.createElement("div");
+	card.classList.add("card");
+	card.setAttribute("id", pizza.id);
+	container.appendChild(card);
 
+	// Crear la imagen cerrar
+	const img = document.createElement("img");
+	img.classList.add("close");
+	img.src = "./assets/img/close.png";
+	card.appendChild(img);
+	// Agregar el evento para eliminar toda la card
+	img.addEventListener("click", () => {
+		card.remove();
+	});
+
+	// Crar la imagen de la card
+	const cardImg = document.createElement("img");
+	cardImg.classList.add("imagen");
+	cardImg.src = pizza.imagen;
+	card.appendChild(cardImg);
+
+	// Crear el card body
+	const cardBody = document.createElement("div");
+	cardBody.classList.add("card-body");
+	card.appendChild(cardBody);
+
+	// Crear el nombre de la pizza
+	const cardNombre = document.createElement("h5");
+	cardNombre.classList.add("name-pizza");
+	cardNombre.innerHTML = pizza.nombre;
+	cardBody.appendChild(cardNombre);
+
+	// Crear titulo ingredientes
+	const cardIngredientes = document.createElement("p");
+	cardIngredientes.classList.add("ingredients");
+	cardIngredientes.innerHTML = "Ingredientes:";
+	cardBody.appendChild(cardIngredientes);
+
+	// Crear lista de ingredientes
+	const cardList = document.createElement("ul");
+	cardList.classList.add("list-ingredients");
+	cardBody.appendChild(cardList);
+
+	// Crear los ingredientes
+	pizza.ingredientes.forEach((ingrediente) => {
+		const cardItem = document.createElement("li");
+		cardItem.innerHTML = ingrediente;
+		cardList.appendChild(cardItem);
+	});
+
+	// Container precios y agregar
+	const cardPrecio = document.createElement("div");
+	cardPrecio.classList.add("container-price");
+	cardBody.appendChild(cardPrecio);
+
+	// Crear el precio
+	const cardPrice = document.createElement("p");
+	cardPrice.classList.add("price");
+	cardPrice.innerHTML = `Precio: ${pizza.precio}`;
+	cardPrecio.appendChild(cardPrice);
+
+	// Crear el boton agregar
+	const cardBtn = document.createElement("button");
+	cardBtn.classList.add("btn-add");
+	cardBtn.innerHTML = "Agregar Carrito";
+	cardPrecio.appendChild(cardBtn);
+};
 
 formulario.addEventListener("submit", (e) => {
-  e.preventDefault();
+	e.preventDefault();
 
-  // Obtener el array de pizzas del localStorage
-  const pizzasLocalStorage = JSON.parse(localStorage.getItem("pizzas"));
+	// Obtener el array de pizzas del localStorage
+	const pizzasLocalStorage = JSON.parse(localStorage.getItem("pizzas"));
+
+	// Encuentra el primer objeto que cumple con el id que se le pasa
+	// https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array/find
+	const pizzaByID = pizzasLocalStorage.find(
+		(pizza) => pizza.id == idPizza.value
+	);
+
+	// Me devuelve un array donde esta la pizza con el id que le paso
+	// const pizzaByID = pizzasLocalStorage.filter((pizza) => pizza.id == idPizza.value);
+
+	// Validaciones
+	if (idPizza.value == "") {
+		alert("Debes ingresar un ID");
+		return;
+	}
+
+	if (pizzaByID == undefined) {
+		alert("No existe la pizza con el ID ingresado");
+		return;
+	}
 
 
-  // Encuentra el primer objeto que cumple con el id que se le pasa
-  // https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array/find
-  const pizzaByID = pizzasLocalStorage.find((pizza) => pizza.id == idPizza.value);
 
-  // Me devuelve un array donde esta la pizza con el id que le paso
-  // const pizzaByID = pizzasLocalStorage.filter((pizza) => pizza.id == idPizza.value);
+	// Mostrar la pizza en la card
+	console.log(pizzaByID);
+	crearCard(pizzaByID);
 
-  // Validaciones
-  if (idPizza.value == "") {
-    alert("Debes ingresar un ID");
-    return;
-  }
+	// Limpiar el formulario
+	formulario.reset();
 
-  if (pizzaByID == undefined) {
-    alert("No existe la pizza con el ID ingresado");
-    return;
-  }
+});
 
-  // Mostrar la pizza en la card
-  console.log(pizzaByID);
-})
+
